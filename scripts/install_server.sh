7dca0255fc8fc34315bdb9b9ac0a3eb6fa15934c
han <88191460+Hanjoohee@users.noreply.github.com>
2024-06-15 21:20:35 +0900

    version 1.0.0

---
+++ scripts/install_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install the server apache2
+sudo apt update
+sudo apt upgrade
+sudo apt install -y apache2
---
+++ scripts/start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Start the web server
+sudo systemctl start apache2
+sudo systemctl enable apache2
